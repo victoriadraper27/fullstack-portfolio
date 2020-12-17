cff7abfcaabcc724992c58ba86490ba53f4a89e6
Victoria Draper <victoria.draper27@gmail.com>
2020-12-17 22:11:57 +0900

    FEAT: Added Animation to Cards

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 
 window.onload = () => {
   $('.slider').slick({
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "dist/bundle.js"
-  },
-  devtool: "sourcemap"
-};
